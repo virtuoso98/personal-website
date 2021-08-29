54d9e9b1e6198ec3180420ed9b4bdfcf839642bd
Koa Zhao Yuan <claydol345@hotmail.com>
2021-08-29 10:40:33 +0800

    final version to be pushed

--- src/components/skeleton/Experiences.jsx
+++ src/components/skeleton/Experiences.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
-import ExperienceCard from '../cards/ExperienceCard';
-import campus from '../../assets/images/campus.jpg';
-import mystery from '../../assets/images/mystery.jpg';
-import nyjc from '../../assets/images/nyjc.png';
-import smartjen from '../../assets/images/smartjen.png';
-import datature from '../../assets/images/datature.jpg'
-import jobkred from '../../assets/images/jobkred.png'
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Grid, Typography } from "@material-ui/core";
+import ExperienceCard from "../cards/ExperienceCard";
+import campus from "../../assets/images/campus.jpg";
+import mystery from "../../assets/images/mystery.jpg";
+import nyjc from "../../assets/images/nyjc.png";
+import smartjen from "../../assets/images/smartjen.png";
+import datature from "../../assets/images/datature.jpg";
+import jobkred from "../../assets/images/jobkred.png";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      paddingTop: '50px',
+      paddingTop: "50px",
     },
     divider: {
-      marginTop: '100px',
+      marginTop: "100px",
     },
-    '@media only screen and (max-width:600px)': {
+    "@media only screen and (max-width:600px)": {
       root: {
-        '& .MuiTypography-h2': {
-          fontSize: '3rem',
+        "& .MuiTypography-h2": {
+          fontSize: "3rem",
         },
       },
-
     },
-  }),
+  })
 );
 
 const Experiences = () => {
@@ -37,9 +36,7 @@ const Experiences = () => {
       <Grid item xs={10} lg={8}>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Typography variant="h2">
-              Experiences
-            </Typography>
+            <Typography variant="h2">Experiences</Typography>
           </Grid>
           <Grid item xs={12} md={6}>
             <ExperienceCard
@@ -47,8 +44,7 @@ const Experiences = () => {
               name="Nanyang Junior College"
               position="GCE 'A' Levels Graduate"
               date="Jan 2015 - Nov 2016"
-              description=
-              "In my stay here. I engaged in olympiads, chess and running of the library. Beyond these opportunities, the community was also great and accepting."
+              description="In my stay here. I engaged in olympiads, chess and running of the library. Beyond these opportunities, the community was also great and accepting."
             />
           </Grid>
           <Grid item xs={12} md={6}>
@@ -57,8 +53,7 @@ const Experiences = () => {
               name="Yale-NUS College"
               position="Undergraduate"
               date="Aug 2019 - May 2023"
-              description=
-              "Liberal Arts student by day, Coder by night. This school gave me free reign to explore modules in Philosophy, Literature and Modern Social Thought."
+              description="Liberal Arts student by day, Coder by night. This school gave me free reign to explore modules in Philosophy, Literature and Modern Social Thought."
             />
           </Grid>
           <Grid item xs={12} md={6}>
@@ -67,7 +62,7 @@ const Experiences = () => {
               name="SmartJen"
               position="R&D Intern"
               date="Dec 2020- Jan 2021"
-              description="I implemented a character segmentation algorithm for handwritten mathematical expressions and deployed a working prototype on Flask. This model achieved 90% accuracy!"
+              description="I implemented a character segmentation algorithm for handwritten mathematical expressions and deployed a working prototype on Flask, which achieved 90% accuracy!"
             />
           </Grid>
           <Grid item xs={12} md={6}>
@@ -76,7 +71,7 @@ const Experiences = () => {
               name="Datature"
               position="Full-stack Engineer Intern"
               date="May 2021 - Jul 2021"
-              description="Worked on various front-end annotator features of main product and Portal, a free-open source app to help evaluate CV Model performance using Next.js, Leaflet.js and Blueprint.js."
+              description="Worked on front-end annotator features of main product and Portal, a free, open-source app which evaluates CV Model performance using Next.js, Leaflet.js and Blueprint.js."
             />
           </Grid>
           <Grid item xs={12} md={6}>
@@ -85,7 +80,7 @@ const Experiences = () => {
               name="JobKred"
               position="Software Engineer Intern"
               date="Aug 2021- Present"
-              description="Currently, I am working on the backend and deployment side of the company's main product using Docker and Kotlin Springboot."
+              description="Currently, I am working on the backend and front-end automated testing side of the company's main product using Cypress.js and Kotlin Springboot."
             />
           </Grid>
           <Grid item xs={12} md={6}>
@@ -104,7 +99,7 @@ const Experiences = () => {
       </Grid>
       <Grid item xs={1} lg={2}></Grid>
     </Grid>
-  )
-}
+  );
+};
 
 export default Experiences;
